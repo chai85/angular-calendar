d5be9b4c1ac3423028a292c8c9d3398d5e95fc75
unknown <n083382@WWHQ6112.global.ual.com>
2016-02-18 11:41:54 -0600

    alignment

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
-angular.module('CalendarApp', [])
-    .directive('calendar', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'calendar.html'
+angular.module('CollabApp', [])
+    .controller("ContactsController", ["$scope", "$window", function ($scope, $window) {
+        function resize() {
+            // to adjust height of widgets
+            var height = window.innerHeight - 50 - 250;
+            // main div height
+            $(".collab").height(window.innerHeight - 100);
+            // contacts list-group height
+            $(".contacts .list-group").height(height / 2);
+            // events panel-body height
+            $(".events .panel-body").height(height / 2);
         }
-    });
+        resize();
+        $(window).on("resize", resize);
+    }]);
